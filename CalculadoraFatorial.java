73650511fdd1312f181a7d07181902e111bc54f0
Rafael Machado Ribeiro <59900469+RafaelMachadoRibeiro@users.noreply.github.com>
2022-10-19 14:41:39 -0300

    Add files via upload

---
+++ CalculadoraFatorial.java
@@ -0,0 +1,28 @@
+package calculadorafatorial;
+import java.util.Scanner;
+public class CalculadoraFatorial {
+    public static void main(String[] args) {
+        
+        String epc = " ";
+        String f1 = "Olá, esse programa foi desenvolvido com o objetivo de calcular o valor do fatorial do múmero que o usuário digitar";
+        String f2 = "Digite um número: ";
+        String f3 = "O valor de ";
+        String f4 = "! é: ";
+        
+        Scanner in = new Scanner (System.in);
+        int numero = 0;
+        int cont = 1;
+        long fatorial = 1;
+        
+        System.out.println(f1);
+        System.out.println(epc);
+        System.out.println(f2);
+        numero = in.nextInt();
+                
+        for (cont = numero; cont > 1; cont--){
+            fatorial = fatorial * cont;
+        } 
+        
+        System.out.println(f3 + numero + f4 + fatorial);
+    }   
+}
